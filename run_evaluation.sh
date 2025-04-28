29efece1667e347c9c0f60fdf27eb66af4ed9d5b
flyhero99 <li.14042@osu.edu>
2025-04-28 00:26:07 +0000

    update

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -4,8 +4,8 @@
 python -m evaluation.harness.run_evaluation \
 --benchmark_path benchmark \
 --pred_program_path pred_programs \
---log_fname eval_qwen2.5-coder-14b_instruct_sft_0425_4omini.jsonl \
---run_id 28 \
+--log_fname eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini.jsonl \
+--run_id 1 \
 --force_rebuild True \
 --cache_level base \
 --max_workers 8

660cd2f1cbfc8cfafaa633db19eb7634237be02c
flyhero99 <li.14042@buckeyemail.osu.edu>
2025-04-27 22:24:25 -0500

    update

---
+++ run_calculate_metrics.sh
@@ -0,0 +1,11 @@
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run1.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run2.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run1.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run2.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run3.jsonl
+
+python calculate_metrics.py \
+    --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run3.jsonl \
+    --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run3.jsonl

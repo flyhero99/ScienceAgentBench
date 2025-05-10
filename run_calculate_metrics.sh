11d825f3604448000c7bac7c6b60c1ed6f8db9f0
flyhero99 <li.14042@buckeyemail.osu.edu>
2025-05-09 21:15:40 -0500

    script change

--- run_calculate_metrics.sh
+++ run_calculate_metrics.sh
@@ -6,6 +6,54 @@
 #     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run2.jsonl \
 #     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run3.jsonl
 
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run3.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen3-32b-0428_run1.jsonl \
+#     --eval_logs eval_qwen3-32b-0428_run1.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run1.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run2.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run1.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run2.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run3.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_h3_y34_0429_run3.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b_instruct_0shot_0427_run1.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_0shot_0427_run2.jsonl \
+#     --run_logs qwen2.5-coder-32b_instruct_0shot_0427_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_0shot_0427_run1.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_0shot_0427_run2.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b_instruct_0shot_0427_run3.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run1.jsonl \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run2.jsonl \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run1.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run2.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_0430_run3.jsonl
+
+# python calculate_metrics.py \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run1.jsonl \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run2.jsonl \
+#     --run_logs qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run3.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run1.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run2.jsonl \
+#     --eval_logs eval_qwen2.5-coder-32b-instruct_full_sft_full_data_expanded_run3.jsonl
+
 python calculate_metrics.py \
-    --run_logs qwen2.5-coder-32b_instruct_full_sft_0427_run3.jsonl \
-    --eval_logs eval_qwen2.5-coder-32b_instruct_full_sft_0427_4omini_run3.jsonl
+    --run_logs qwen2.5-coder-32b-instruct_bs64_ep1_run1.jsonl \
+    --run_logs qwen2.5-coder-32b-instruct_bs64_ep1_run2.jsonl \
+    --run_logs qwen2.5-coder-32b-instruct_bs64_ep1_run3.jsonl \
+    --eval_logs eval_qwen2.5-coder-32b-instruct_bs64_ep1_run1.jsonl \
+    --eval_logs eval_qwen2.5-coder-32b-instruct_bs64_ep1_run2.jsonl \
+    --eval_logs eval_qwen2.5-coder-32b-instruct_bs64_ep1_run3.jsonl
